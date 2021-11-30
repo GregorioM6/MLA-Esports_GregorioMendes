2c3501dc21c95190bb487041ce7b87d4580a4038
GregorioM6 <87047652+GregorioM6@users.noreply.github.com>
2021-11-30 00:02:27 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
 
@@ -23,7 +23,6 @@ export default App;
 //     <div className="App">
 //       <header className="App-header">
 //         <div className="App">
-          
 //         <img src={logo} className="App-logo" alt="logo" />
 //         <p>
 //           Edit <code>src/App.js</code> and save to reload.
