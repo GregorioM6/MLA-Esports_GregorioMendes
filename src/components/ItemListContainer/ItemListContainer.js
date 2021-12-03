44504d423ff6dfac76b47016e05f05fe4f6728d9
GregorioM6 <87047652+GregorioM6@users.noreply.github.com>
2021-12-02 23:53:47 -0300

    boton clase 5

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,32 +1,52 @@
-import Item from '../Item/Item';
+// import Item from '../Item/Item';
+import { ItemCount } from '../ItemCount/ItemCount';
 
-const ItemListContainer = () => {
-    const dataProducts = [
-        {
-            name: 'Monitor Gamer',
-            price: 60000,
-            stock: 15,
-            color: 'negro', 
-            img: 'monitorgamer.jpg'
-        },
-        {
-            name: 'Mouse Gamer',
-            price: 6000,
-            stock: 20,
-            color: 'negro', 
-            img: 'mousegamer.png'
-        }
-    ]
+// const ItemListContainer = () => {
+//     const dataProducts = [
+//         {
+//             name: 'Monitor Gamer',
+//             price: 60000,
+//             stock: 15,
+//             color: 'negro', 
+//             img: 'monitorgamer.jpg'
+//         },
+//         {
+//             name: 'Mouse Gamer',
+//             price: 6000,
+//             stock: 20,
+//             color: 'negro', 
+//             img: 'mousegamer.png'
+//         }
+//     ]
 
-    return(
+//     return(
+//         <div>
+//             <h2>Productos MLA</h2>
+//             {dataProducts.map((product)=> {
+//                 return(
+//                     <Item key={product.name} name={product.name} price={product.price} color={product.color} stock={product.Stock} img={product.img}/>
+//             )})}
+//         </div>
+//     )
+// }
+
+
+export const ItemListContainer = ({ Cantidad }) => {
+
+    const onAdd = (amount) => {
+        console.log(amount);
+    }
+
+    return (
         <div>
-            <h2>Productos MLA</h2>
-            {dataProducts.map((product)=> {
-                return(
-                    <Item key={product.name} name={product.name} price={product.price} color={product.color} stock={product.Stock} img={product.img}/>
-            )})}
+            <h1>{ Cantidad }</h1>
+            <ItemCount 
+                stock = {10}
+                initial = {1}
+                onAdd = {onAdd}
+            />
         </div>
-    )
+    );
 }
 
 export default ItemListContainer
